14a736128add7d672291089ba7a613401cb8c675
Sutrishna Bhunia <134145442+sutrishnabhunia@users.noreply.github.com>
2023-07-13 22:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    let result;
+    for (let i = 2; i <= num; i++) {
+      result = a + b;
+      a = b;
+      b = result;
+    }
+    return result;
+  }
 }
-
-module.exports = fibonacci;
